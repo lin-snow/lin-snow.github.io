18e6923d029eaff7ee8f35782e9a60eb0f7b857f
L1nsn0w <1909901779@qq.com>
2023-10-01 16:10:04 +0800

    homepage update

---
+++ README.md
@@ -0,0 +1,3 @@
+这里是L1nSn0w的主页，由jimmycai.com的网站copy而来.
+
+https://linsnow.cn
---
+++ deploy.sh
@@ -0,0 +1,7 @@
+git init
+
+git add -A
+
+git commit -m 'homepage update'
+
+git push -f https://github.com/lin-snow/lin-snow.github.io.git main
